55f1f7b2d02133e28e5fec0004ef9cf5aac1e7a3
Test User <test@mail>
2020-07-17 14:55:51 +0200

    Test Notify Push.js

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>NOTIFICATION</title>
+	<script src="js/push.min.js"></script>
+</head>
+<body>
+	<script>
+		Push.create("Hello Jerk");
+	</script>
+</body>
+</html>
