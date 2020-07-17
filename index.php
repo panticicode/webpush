8fd8c439f73cf3e611d911c198079d636fa20876
Test User <test@mail>
2020-07-17 15:13:02 +0200

    fix

--- index.php
+++ index.php
@@ -3,11 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<title>NOTIFICATION</title>
-	<script src="js/push.min.js"></script>
+	
 </head>
 <body>
-	<script>
-		Push.create("Hello Jerk");
-	</script>
+	asdasd
 </body>
 </html>
